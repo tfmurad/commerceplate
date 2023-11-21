c0ea9b0a3c022410a7c3422e21bc7a5d7d27aaf4
Al Murad Uzzaman <murad.themefisher@gmail.com>
2023-11-21 16:02:35 +0600

    image swatches with image added

--- src/layouts/components/product/variant-selector.tsx
+++ src/layouts/components/product/variant-selector.tsx
@@ -2,9 +2,11 @@
 
 import { ProductOption, ProductVariant } from "@/lib/shopify/types";
 import { createUrl } from "@/lib/utils";
+import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { BsCheckLg } from "react-icons/bs";
 import VariantDropDown from "./VariantDropDown";
+import { BsCheckLg } from "react-icons/bs";
+import { ImageItem } from "./ProductGallery";
 
 type Combination = {
   id: string;
@@ -12,16 +14,33 @@ type Combination = {
   [key: string]: string | boolean;
 };
 
+export const generateImageMap = (images: ImageItem[]) => {
+  const imageMap: { [altText: string]: string } = {};
+
+  images.forEach((image) => {
+    // Use the first image encountered for each unique altText
+    if (!(image.altText in imageMap)) {
+      imageMap[image.altText] = image.url;
+    }
+  });
+
+  return imageMap;
+};
+
 export function VariantSelector({
   options,
   variants,
+  images
 }: {
   options: ProductOption[];
   variants: ProductVariant[];
+  images: any
 }) {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  const imageMap = generateImageMap(images);
 
   const hasNoOptionsOrJustOneOption =
     !options.length ||
@@ -46,103 +65,95 @@ export function VariantSelector({
   // Find the 'Size' option
   const sizeOption = options.find((option) => option.name === "Size");
 
-  return (
-    <div>
-      {options.map((option) => (
-        <div className="" key={option.id}>
-          <h5 className="mb-2 max-md:text-base">
-            {option.name === "Size" ? "" : option.name}
-          </h5>
-          <div className="flex flex-wrap gap-3">
-            {option.values.map((value) => {
-              const optionNameLowerCase = option.name.toLowerCase();
-              const optionSearchParams = new URLSearchParams(
-                searchParams.toString(),
-              );
-              optionSearchParams.set(optionNameLowerCase, value);
-              const optionUrl = createUrl(pathname, optionSearchParams);
+return (
+  <div>
+    {options.map((option) => (
+      <div className="" key={option.id}>
+        <h5 className="mb-2 max-md:text-base">
+          {option.name === "Size" ? "" : option.name}
+        </h5>
+        <div className="flex flex-wrap gap-3">
+          {option.values.map((value) => {
+            const optionNameLowerCase = option.name.toLowerCase();
+            const optionSearchParams = new URLSearchParams(
+              searchParams.toString(),
+            );
+            optionSearchParams.set(optionNameLowerCase, value);
+            const optionUrl = createUrl(pathname, optionSearchParams);
 
-              const filtered = Array.from(optionSearchParams.entries()).filter(
-                ([key, value]) =>
-                  options?.find(
-                    (option) =>
-                      option.name.toLowerCase() === key &&
-                      option.values.includes(value),
-                  ),
-              );
-
-              const isAvailableForSale = combinations.find((combination) =>
-                filtered.every(
-                  ([key, value]) =>
-                    combination[key] === value && combination.availableForSale,
+            const filtered = Array.from(optionSearchParams.entries()).filter(
+              ([key, value]) =>
+                options?.find(
+                  (option) =>
+                    option.name.toLowerCase() === key &&
+                    option.values.includes(value),
                 ),
-              );
+            );
 
-              const isActive = searchParams.get(optionNameLowerCase) === value;
+            const isAvailableForSale = combinations.find((combination) =>
+              filtered.every(
+                ([key, value]) =>
+                  combination[key] === value && combination.availableForSale,
+              ),
+            );
 
-              if (option.name === "Size") {
-                return null; // skip rendering in the loop
-              }
+            const isActive = searchParams.get(optionNameLowerCase) === value;
 
-              return (
-                <div key={value} className="mb-2">
-                  <button
-                    key={value}
-                    aria-disabled={!isAvailableForSale}
-                    disabled={!isAvailableForSale}
-                    onClick={() => {
-                      router.replace(optionUrl, { scroll: false });
-                    }}
-                    title={`${option.name} ${value}${
-                      !isAvailableForSale ? " (Out of Stock)" : ""
-                    }`}
-                    className={`flex min-w-[48px] items-center justify-center rounded-md border ${
-                      option.name === "Color"
-                        ? "bg-transparent border-none"
-                        : "bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-900 px-2 py-1"
-                    } text-sm ${
-                      isActive && option.name !== "Color"
-                        ? "cursor-default ring-2 ring-dark dark:ring-darkmode-dark"
-                        : ""
-                    } ${
-                      !isActive && isAvailableForSale && option.name !== "Color"
-                        ? "ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-dark hover:dark:ring-darkmode-dark"
-                        : ""
-                    } ${
-                      !isAvailableForSale
-                        ? "relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500  before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700"
-                        : ""
-                    }`}
-                  >
-                    {option.name == "Color" ? (
-                      <span
-                        className={`h-10 w-10 rounded-md`}
-                        style={{ backgroundColor: `#${value}` }}
-                      >
-                        {isActive && (
-                          <span className="text-white flex items-center justify-center h-full">
-                            <BsCheckLg size={25} />
+            if (option.name === "Size") {
+              return null; // skip rendering in the loop
+            }
+
+            return (
+              <div key={value} className="mb-2">
+                <button
+                  key={value}
+                  aria-disabled={!isAvailableForSale}
+                  disabled={!isAvailableForSale}
+                  onClick={() => {
+                    router.replace(optionUrl, { scroll: false });
+                  }}
+                  title={`${option.name} ${value}${
+                    !isAvailableForSale ? " (Out of Stock)" : ""
+                  }`}
+                  className={`flex min-w-[48px] items-center justify-center rounded-md border text-sm ${isActive && option.name !== "Color"
+                    ? "cursor-default ring-2 ring-dark dark:ring-darkmode-dark"
+                    : ""
+                  } ${!isActive && isAvailableForSale && option.name !== "Color"
+                    ? "ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-dark hover:dark:ring-darkmode-dark"
+                    : ""
+                  } ${!isAvailableForSale
+                    ? "relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500  before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700"
+                    : ""
+                  }`}
+                >
+                  {/* Render the color image for the current value */}
+                  {option.name === "Color" ? (
+                    <div key={value} className={`relative rounded-md overflow-hidden ${isActive && "border border-dark dark:border-darkmode-dark outline-none"}`}>
+                      <Image src={imageMap[value]} alt={value} width={50} height={50} />
+                      {isActive && (
+                          <span className="text-inherit h-full opacity-100 absolute top-2 right-2">
+                            <BsCheckLg size={35} />
                           </span>
                         )}
-                      </span>
-                    ) : (
-                      value
-                    )}
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+                    </div>
+                  ): value} 
+                  
+                </button>
+              </div>
+            );
+          })}
         </div>
-      ))}
+      </div>
+    ))}
 
-      {/* rendering the 'Size' select element outside the loop */}
-      {sizeOption && (
-        <div className="mb-8">
-          <h5 className="mb-2 max-md:text-base">{sizeOption.name}</h5>
-          <VariantDropDown sizeOption={sizeOption} options={options} />
-        </div>
-      )}
-    </div>
-  );
+    {/* rendering the 'Size' select element outside the loop */}
+    {sizeOption && (
+      <div className="mb-8">
+        <h5 className="mb-2 max-md:text-base">{sizeOption.name}</h5>
+        <VariantDropDown sizeOption={sizeOption} options={options} />
+      </div>
+    )}
+  </div>
+);
+
 }
