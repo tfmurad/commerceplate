38563f034e32af960a0b043a2b66b1b58d161fac
Al Murad Uzzaman <murad.themefisher@gmail.com>
2023-12-04 16:00:36 +0600

    search highlighted

--- src/layouts/components/product/ProductLayouts.tsx
+++ src/layouts/components/product/ProductLayouts.tsx
@@ -9,8 +9,8 @@ import { useCollapse } from "react-collapsed";
 import { BsGridFill } from "react-icons/bs";
 import { FaList } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
+import { TbFilter, TbFilterX } from "react-icons/tb";
 import DropdownMenu from "../filter/DropdownMenu";
-import { TbFilter,TbFilterX } from "react-icons/tb";
 
 export type ListItem = SortFilterItem | PathFilterItem;
 export type PathFilterItem = { title: string; path: string };
@@ -98,7 +98,6 @@ const ProductLayouts = ({ categories, vendors, tags, maxPriceData }: any) => {
           <div className="row gy-4">
             <div className="col-12 lg:col-3">
               <div className=" lg:block relative">
-                
                 {/* <div className="block lg:hidden">
                   <PopoverFilter categories={categories}
                     vendors={vendors}
@@ -110,9 +109,13 @@ const ProductLayouts = ({ categories, vendors, tags, maxPriceData }: any) => {
                   <div className="filter-button-container mb-4">
                     <button {...getToggleProps()}>
                       {isExpanded ? (
-                         <span className="font-medium text-base flex gap-x-1 items-center justify-center"><TbFilterX /> Filter</span>
+                        <span className="font-medium text-base flex gap-x-1 items-center justify-center">
+                          <TbFilterX /> Filter
+                        </span>
                       ) : (
-                        <span className="font-medium text-base flex gap-x-1 items-center justify-center"><TbFilter /> Filter</span>
+                        <span className="font-medium text-base flex gap-x-1 items-center justify-center">
+                          <TbFilter /> Filter
+                        </span>
                       )}
                     </button>
                   </div>
@@ -129,25 +132,28 @@ const ProductLayouts = ({ categories, vendors, tags, maxPriceData }: any) => {
                   </section>
                 </div>
 
-                <form
-                  onSubmit={onSubmit}
-                  className={`border border-border dark:border-darkmode-border rounded-md flex max-lg:absolute right-0 -top-2`}
-                >
-                  <input
-                    id="searchInput"
-                    className="bg-transparent border-none focus:ring-transparent pr-0 pl-2 w-full"
-                    key={searchParams?.get("q")}
-                    type="text"
-                    name="search"
-                    placeholder="Search for products..."
-                    autoComplete="off"
-                    defaultValue={searchParams?.get("q") || ""}
-                    onChange={handleChange}
-                  />
-                  <button className="pr-2 search-icon">
-                    <IoSearch size={20} />
-                  </button>
-                </form>
+                <div>
+                  <h5 className="mb-2 lg:text-xl max-lg:hidden">Search</h5>
+                  <form
+                    onSubmit={onSubmit}
+                    className={`border border-border dark:border-darkmode-border rounded-md flex max-lg:absolute right-0 -top-2`}
+                  >
+                    <input
+                      id="searchInput"
+                      className="bg-transparent border-none focus:ring-transparent pr-0 pl-2 w-full"
+                      key={searchParams?.get("q")}
+                      type="text"
+                      name="search"
+                      placeholder="Search for products"
+                      autoComplete="off"
+                      defaultValue={searchParams?.get("q") || ""}
+                      onChange={handleChange}
+                    />
+                    <button className="pr-2 search-icon">
+                      <IoSearch size={20} />
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
 
@@ -156,17 +162,15 @@ const ProductLayouts = ({ categories, vendors, tags, maxPriceData }: any) => {
                 <div className="flex gap-2">
                   <button
                     onClick={() => layoutChange("card")}
-                    className={`btn border dark:border-darkmode-border ${
-                      isListView ? "btn-outline-primary" : "btn-primary"
-                    } p-2 hover:scale-105 duration-300`}
+                    className={`btn border dark:border-darkmode-border ${isListView ? "btn-outline-primary" : "btn-primary"
+                      } p-2 hover:scale-105 duration-300`}
                   >
                     <BsGridFill />
                   </button>
                   <button
                     onClick={() => layoutChange("list")}
-                    className={`btn border dark:border-darkmode-border ${
-                      isListView ? "btn-primary" : "btn-outline-primary"
-                    } p-2 hover:scale-105 duration-300`}
+                    className={`btn border dark:border-darkmode-border ${isListView ? "btn-primary" : "btn-outline-primary"
+                      } p-2 hover:scale-105 duration-300`}
                   >
                     <FaList />
                   </button>
